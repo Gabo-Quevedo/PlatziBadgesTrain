d6f9ed3fcad50f045e35ff78eaef75462d8cf1f5
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-07-14 13:55:22 -0400

    Se terminan los campos del fomrulario con inputs controlados por state

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 class BadgeForm extends React.Component{
+    state = {}
     handleChange = (e)=>{
-        console.log({
-            name: e.target.name,
-            value: e.target.value})
+        // console.log({
+        //     name: e.target.name,
+        //     value: e.target.value})
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
     }
     handleClick = ()=>{
         console.log('Click')
@@ -12,6 +16,7 @@ class BadgeForm extends React.Component{
     handleSubmit = e =>{
         e.preventDefault()
         console.log('Form was submited')
+        console.log(this.state)
     }
     render() {
         return(
@@ -21,7 +26,48 @@ class BadgeForm extends React.Component{
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange} className="form-control" name="firstName" type="text"/>
+                        <input 
+                            onChange={this.handleChange} 
+                            className="form-control" 
+                            name="firstName"
+                            type="text"
+                            value={this.state.firstName}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input 
+                            onChange={this.handleChange} 
+                            className="form-control" 
+                            name="lastName"
+                            type="text"
+                            value={this.state.lastName}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input 
+                            onChange={this.handleChange} 
+                            className="form-control" 
+                            name="email"
+                            type="text"
+                            value={this.state.email}/>
+                    </div>
+                    <div className="form-group">
+                        <label>JobTitle</label>
+                        <input 
+                            onChange={this.handleChange} 
+                            className="form-control" 
+                            name="jobTitle"
+                            type="text"
+                            value={this.state.jobTitle}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input 
+                            onChange={this.handleChange} 
+                            className="form-control" 
+                            name="twitter"
+                            type="text"
+                            value={this.state.twitter}/>
                     </div>
                     <button onClick={this.handleClick} className="btn btn-primary">Save</button>
                 </form>
