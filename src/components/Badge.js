32a356b6b2b7282a4ca9b68a14fcf73fdc7c8362
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-07-14 11:59:50 -0400

    Se agregan hojas de esilo del proyecto y se termina el componente badge

---
+++ src/components/Badge.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import './styles/Badge.css'
+import confLogo from '../images/badge-header.svg'
+
+class Badge extends React.Component{
+    render(){
+        return(
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={confLogo} alt="Logo"/>
+                </div>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="http://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+                    <h1> Gabo <br/> Quevedo</h1>
+                </div>
+                <div className="Badge__section-info">
+                    <h3>Frontend developer</h3>
+                    <div>@gabofunk</div>
+                </div>
+                <div className="Badge__footer">
+                    #platziconf
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Badge
