fdac48686a0f98c346e3c53d359cf1cdb665330e
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-07-15 03:25:00 -0400

    Se agrega Loader PageError con css respectivos controlados según la petición a la api

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class BadgesList extends React.Component{
    render(){
+       if(this.props.badges.lenght===0){
+           return(
+               <div>
+                   <h3>No se encontró Nara</h3>
+               <Link className="btn btn-primary" to="/badges/new">
+                   Create NewBadge
+               </Link>
+               </div>
+           )
+       }
        return(
             <ul className="list-unstyled">
                 {this.props.badges.map((badge)=>{
